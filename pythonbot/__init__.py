d20fcee65872d5560bc38ede13dbee2644bed2ad
Hyper-Jai <134952449+Hyper-Jai@users.noreply.github.com>
2023-06-13 10:37:41 +0530

    __init__.py

--- pythonbot/__init__.py
+++ pythonbot/__init__.py
@@ -5,6 +5,5 @@ import time
 openai_key = "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"
 api_id = "26826728"
 api_hash = "35ca28ae08aa543bfcaba98649a7a580"
-bot_token = "6034653584:AAGi6BTgIJwMp43Dro4YMFUb0CtRy5jA_e4"
-
+bot_token = "6034653584:AAGof6UAXEtMCofOzGK_yRl5ygcwOWHzZx8"
 bot = TelegramClient("pythonbot", api_id, api_hash).start(bot_token=bot_token)
